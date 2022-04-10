579c3a58e3805d1872645000128adb4856b94a04
chineduisraele <chineduisraele@gmail.com>
2022-04-10 15:02:07 +0100

    added notify me signal

---
+++ message/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django.core.mail import send_mail
+from app.settings import EMAIL_HOST_USER
+
+from message.models import Message
+
+
+@receiver(post_save, sender=Message)
+def NotifyMe(sender, instance, created, *args, **kwargs):
+    if created:
+        message = f'{instance.email} sent you a message. {instance.message}'
+        send_mail("PORTFOLIO NEW MESSAGE", message,
+                  EMAIL_HOST_USER, "chineduie01@gmail.com")
