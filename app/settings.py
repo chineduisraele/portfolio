5b47c55676e7d4f7c29f604d71dd62fa9960a7e7
chineduisraele <chineduisraele@gmail.com>
2022-04-10 18:13:16 +0100

    //

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 # Initialise environment variables
@@ -133,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = BASE_DIR/'staticfiles/'
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # media
 MEDIA_URL = 'media/'
