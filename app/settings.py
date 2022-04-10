9f877dea8e3a5fcf1a707bd3a4bf4fbb9929b531
chineduisraele <chineduisraele@gmail.com>
2022-04-10 23:27:06 +0100

    installed psycopg2. updated database to postgre

--- app/settings.py
+++ app/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'USERNAME': env('DB_USERNAME'),
+        'PASSWORD': env('DB_PASSWORD')
     }
 }
 
